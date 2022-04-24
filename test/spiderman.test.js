533b65d52b86907281496412484105130d4d0723
AnethArmenta <anethalvarezza@gmail.com>
2022-04-23 20:49:38 -0500

    6. Actualizar spiderman.test.js

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,13 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../app/spiderman')
+describe("Test for spiderman", () => {
+    test('Create spiderman object', () => {
+      const Tomm = new Spiderman("spiderman 1",27,"Tomm",10,"Disney")
+
+      expect(Tomm.nombre).toBe("spiderman 1")
+      expect(Tomm.edad).toBe(27)
+      expect(Tomm.actor).toBe("Tomm")
+      expect(Tomm.peliculas).toBe(10)
+      expect(Tomm.estudios).toBe("Disney")
+      
     });
   })
