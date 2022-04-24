05ce11ed03327901313383f73d302542bf6f805b
AnethArmenta <anethalvarezza@gmail.com>
2022-04-23 20:50:53 -0500

    7. Actualizando spiderman.js

---
+++ app/spiderman.js
@@ -0,0 +1,11 @@
+class Spiderman{
+    constructor(nombre,edad,actor,peliculas,estudios) {
+        this.nombre = nombre,
+        this.edad = edad,
+        this.actor = actor,
+        this.peliculas = peliculas,
+        this.estudios = estudios
+
+      }
+    }
+module.exports = Spiderman
